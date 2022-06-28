155b9478e33bedac304e89437134c8b985661704
Gabriel Moreira <silgabrielmoreira@gmail.com>
2022-06-28 20:23:06 -0300

    Add button "Clear Maze" in MazeCustomInspector

--- TP - SBGames/Assets/Scripts/Inspectors/MazeGeneratorInspector.cs
+++ TP - SBGames/Assets/Scripts/Inspectors/MazeGeneratorInspector.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using UnityEditor;
-namespace Maze_Generator{
+
+namespace Maze_Generator
+{
     [CustomEditor(typeof(TilemapMazeGenerator))]
-    public class MazeGeneratorInspector : Editor{
-        public override void OnInspectorGUI(){
+    public class MazeGeneratorInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
             TilemapMazeGenerator tilemapMazeGenerator = (TilemapMazeGenerator)target;
             base.OnInspectorGUI();
-            if (GUILayout.Button("Generate Maze")){
+
+            if (GUILayout.Button("Generate Maze"))
+            {
                 tilemapMazeGenerator.wallTilemap.ClearAllTiles();
                 tilemapMazeGenerator.groundTilemap.ClearAllTiles();                    
                 tilemapMazeGenerator.GenerateTileMapMaze();
+            }
+
+            if(GUILayout.Button("Clear Maze"))
+            {
+                tilemapMazeGenerator.wallTilemap.ClearAllTiles();
+                tilemapMazeGenerator.groundTilemap.ClearAllTiles();
             }
         }
     }
