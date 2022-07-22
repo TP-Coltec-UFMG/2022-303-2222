2365a20b12739f431ace65a9c06bd2ea97d58e1e
Gabriel Moreira <silgabrielmoreira@gmail.com>
2022-07-22 02:27:12 -0300

    feat: player move and sound localization
    player move based on grid and sound localization improvement

--- TP - SBGames/Assets/Scripts/Player/PlayerMovement.cs
+++ TP - SBGames/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,30 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Range(0, 1000)] [SerializeField] float speed;
-    Rigidbody2D rig;
-    float xInput, yInput;
-    Vector2 moveDir;
-
+    [Range(0, 10)] [SerializeField] float speed;
+    [SerializeField] Transform movePoint;
+    [SerializeField] LayerMask wallCollisionLayer;
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        movePoint.parent = null;
     }
 
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-        yInput = Input.GetAxisRaw("Vertical");
+        float xAxisInput = Input.GetAxisRaw("Horizontal");
+        float yAxisInput = Input.GetAxisRaw("Vertical");
 
-        moveDir = new Vector2(xInput, yInput);
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(xAxisInput, 0f, 0f), .2f, wallCollisionLayer))
+            {
+                if((Mathf.Abs(xAxisInput) == 1f))
+                {
+                    movePoint.position += new Vector3(xAxisInput, 0f, 0f);
+                    do{
+                        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
+                    }while(Vector3.Distance(transform.position, movePoint.position) > .05f);
+                }
+            }
+            
+            if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, yAxisInput, 0f), .2f, wallCollisionLayer))
+            {
+                if(Mathf.Abs(yAxisInput) == 1f)
+                {
+                    movePoint.position += new Vector3(0f, yAxisInput, 0f); 
+                    do{
+                        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
+                    }while(Vector3.Distance(transform.position, movePoint.position) > .05f);             
+                }
+            }
+        }
     }
 
-    void FixedUpdate() 
-    {
-        rig.velocity = moveDir * speed * Time.deltaTime;    
-    }
+
 }
--- TP - SBGames/Assets/Scripts/SoundLocalization/SoundLocalization.cs
+++ TP - SBGames/Assets/Scripts/SoundLocalization/SoundLocalization.cs
@@ -7,33 +7,110 @@ public class SoundLocalization : MonoBehaviour
     [SerializeField] AudioSource upAudio;
     [SerializeField] AudioSource downAudio;
     RaycastHit2D rightRay, leftRay, upRay, downRay;
-    [SerializeField][Range(0,2)] float rayDistance = 2f;
-
+    [SerializeField] float rayDistance = 2f;
+    Vector2 rightPivot, leftPivot, upPivot, downPivot;
+    [SerializeField] AnimationCurve volumeDistanceCurve;
+    
     void Start()
     {
         Physics2D.IgnoreLayerCollision(2,2);
     } 
 
     void Update()
-    {
-        rightRay = Physics2D.Raycast(transform.position, transform.right, rayDistance);
-        leftRay = Physics2D.Raycast(transform.position, - transform.right, rayDistance);
-        upRay = Physics2D.Raycast(transform.position, transform.up, rayDistance);
-        downRay = Physics2D.Raycast(transform.position, - transform.up, rayDistance);
-
-        DebugRay();
+    {        
+        rightPivot = new Vector2(transform.position.x + 0.25f, transform.position.y); 
+        leftPivot = new Vector2(transform.position.x - 0.25f, transform.position.y);
+        upPivot = new Vector2(transform.position.x, transform.position.y + 0.25f);
+        downPivot = new Vector2(transform.position.x, transform.position.y - 0.25f);
         
-        if(rightRay.collider != null) if(!rightAudio.isPlaying) rightAudio.Play();
-        if(leftRay.collider != null) if(!leftAudio.isPlaying) leftAudio.Play();
-        if(upRay.collider != null) if(!upAudio.isPlaying) upAudio.Play();
-        if(downRay.collider != null) if(!downAudio.isPlaying) downAudio.Play();
+        rightRay = Physics2D.Raycast(rightPivot, transform.right, rayDistance);
+        leftRay = Physics2D.Raycast(leftPivot, - transform.right, rayDistance);
+        upRay = Physics2D.Raycast(upPivot, transform.up, rayDistance);
+        downRay = Physics2D.Raycast(downPivot, - transform.up, rayDistance);
+
+        // AudioController();
     }
 
-    void DebugRay()
+    void AudioController()
+    {         
+        if(rightRay.collider != null) 
+        {
+            if(rightRay.distance < rayDistance)
+            {
+                rightAudio.volume = 1 - rightRay.distance/rayDistance;
+            }
+            else
+            {
+                rightAudio.Stop();
+            }
+
+            if(!rightAudio.isPlaying)
+            {
+                rightAudio.Play();
+            } 
+        }
+
+        if(leftRay.collider != null) 
+        {
+            if(leftRay.distance < rayDistance)
+            {
+                leftAudio.volume = 1 - leftRay.distance/rayDistance;
+            }
+            else
+            {
+                leftAudio.Stop();
+            }
+
+            if(!leftAudio.isPlaying)
+            {
+                leftAudio.Play();
+            } 
+        }
+
+        if(upRay.collider != null) 
+        {
+            if(upRay.distance < rayDistance)
+            {
+                upAudio.volume = 1 - upRay.distance/rayDistance;
+            }
+            else
+            {
+                upAudio.Stop();
+            }
+
+            if(!upAudio.isPlaying)
+            {
+                upAudio.Play();
+            } 
+        }
+
+
+        if(downRay.collider != null) 
+        {
+            if(downRay.distance < rayDistance)
+            {
+                downAudio.volume = 1 - downRay.distance/rayDistance;
+            }
+            else
+            {
+                downAudio.Stop();
+            }
+
+            if(!downAudio.isPlaying)
+            {
+                downAudio.Play();
+            } 
+        }
+
+    } 
+    
+    void OnDrawGizmos()
     {
-        Debug.DrawRay(transform.position, transform.right * rayDistance, Color.red);
-        Debug.DrawRay(transform.position, - transform.right * rayDistance, Color.red);
-        Debug.DrawRay(transform.position, transform.up * rayDistance, Color.red);
-        Debug.DrawRay(transform.position, - transform.up * rayDistance, Color.red);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(new Vector2(transform.position.x + 0.25f, transform.position.y), transform.right * rayDistance);
+        Gizmos.DrawRay(new Vector2(transform.position.x - 0.25f, transform.position.y), - transform.right * rayDistance);
+        Gizmos.DrawRay(new Vector2(transform.position.x, transform.position.y + 0.25f), transform.up * rayDistance);
+        Gizmos.DrawRay(new Vector2(transform.position.x, transform.position.y - 0.25f), - transform.up * rayDistance);
     }
+
 }
