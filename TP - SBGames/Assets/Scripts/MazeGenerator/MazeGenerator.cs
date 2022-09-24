bf251049c4877e0a45c708cf29c33e81d179589a
Jean Berly <jeanberlyy@gmail.com>
2022-09-24 16:42:53 -0300

    fix: audio problems on exit ExitSound.cs and SoundLocalization.cs

--- TP - SBGames/Assets/Scripts/MazeGenerator/MazeGenerator.cs
+++ TP - SBGames/Assets/Scripts/MazeGenerator/MazeGenerator.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Maze_Generator
 {
